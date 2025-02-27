454c9cd9bb8642f28086f9c6d42950f4524b7b12
Adam Lithgow <43798347+AdamLithgow@users.noreply.github.com>
2025-02-27 13:51:12 -0500

    Fix time converter

--- OrToolsBugDemo/Utilities/TimeConverter.cs
+++ OrToolsBugDemo/Utilities/TimeConverter.cs
@@ -13,7 +13,10 @@ public class TimeConverter
 	public long ConvertToSeconds(DateTime dateTime)
 	{
 		var ms = new DateTimeOffset(dateTime).ToUnixTimeMilliseconds();
+
+		if (_quotient != 0) return (long)(ms - (Math.Pow(10, 9) * _quotient)) / 1000;
+
 		_quotient = Math.DivRem(ms, (long)Math.Pow(10, 9), out var msNoPrefix);
-		return (msNoPrefix / 1000);
+		return msNoPrefix / 1000;
 	}
 }
